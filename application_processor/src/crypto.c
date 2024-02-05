56a1bbc2ee594044e0a35af0ae7210ec4e167db6
Andrew Kingery <kandrew2022@gmail.com>
2024-02-05 17:27:54 -0500

    Add AES CTR Decryption

--- application_processor/src/crypto.c
+++ application_processor/src/crypto.c
@@ -9,7 +9,7 @@ int encrypt_sym(uint8_t *plaintext, size_t len, uint8_t *key, uint8_t *ciphertex
     
     // nonce used for Initialization Vector
     uint8_t nonce[AES_BLOCK_SIZE];
-    
+
     // Generate Random Number Generator (Pseudo-Random)
     // TODO: make this true random w/ some GPIO data or something
     ret = wc_InitRng(^rng);
@@ -17,7 +17,7 @@ int encrypt_sym(uint8_t *plaintext, size_t len, uint8_t *key, uint8_t *ciphertex
         fprintf(stderr, "Could not initialize random number generator.");
         return -1;
     }
-    
+
     // Generate pseudo-random nonce
     ret = wc_RNG_GenerateBlock(&rng, iv, AES_BLOCK_SIZE);
     if (ret != 0) {
@@ -38,7 +38,7 @@ int encrypt_sym(uint8_t *plaintext, size_t len, uint8_t *key, uint8_t *ciphertex
         fprintf(stderr, "Could not set AES key");
         return -4;
     }
-    
+
     // Encrypt w/ AES-CTR
     ret = wc_AesCtrEncrypt(&ctx, ciphertext, plaintext, len);
     if (ret != 0) {
@@ -46,8 +46,43 @@ int encrypt_sym(uint8_t *plaintext, size_t len, uint8_t *key, uint8_t *ciphertex
         return -5;
     }
     
-    // free rng
+    // Cleanup
+    memset(key, 0, 64);
     wc_FreeRng(&rng);
     
     return ret;
+}
+
+int decrypt_sym(uint8_t *iv_cipher, size_t len, uint8_t *key, uint8_t *plaintext) {
+    Aes ctx;
+    int ret;
+    
+    uint8_t iv = iv_cipher;
+    uint8_t ciphertext = iv_cipher + AES_BLOCK_SIZE;
+    
+    // Initialize AES struct
+    ret = wc_AesInit(aes, NULL, INVALID_DEVID);
+    if (ret != 0) {
+        fprintf(stderr, "Could not initialize AES Struct");
+        return -1;
+    }
+    
+    // Set the key
+    // Decryption for CTR still uses the AES_ENCRYPTION
+    ret = wc_AesSetKey(aes, key, 64, iv, AES_ENCRYPTION);
+    if (ret != 0) {
+        fprintf(stderr, "Could not set key");
+        return -2;
+    }
+
+    // Decrypt
+    // note: AesCtrEncrypt both encrypts and decrypts
+    ret = wc_AesCtrEncrypt(aes, plaintext, ciphertext, len);
+    if (ret != 0) {
+        fprintf(stderr, "Could not decrypt");
+        return -3;
+    }
+
+    // Cleanup
+    memset(key, 0, 64);
 }
